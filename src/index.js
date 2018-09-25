e49fadc6ab3bf84a1d211998d86674bf7e4bbb5f
Pulya10c <sergienya.oleg@yandex.ru>
2018-09-25 15:47:14 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
 
+  class SetNum  {
+    constructor () {
+      this.arr = [];
+      this.num = 2;
+      
+    }
+    getNum (x) {
+      if (x === '1') {
+          this.arr.push(1);
+          }
+      if(x % this.num === 0){
+       this.arr.push(this.num);
+       x = x / this.num;
+       this.getNum(x);
+       //alert (this.arr);
+    
+    }else if(x % this.num !== 0){
+      if(!(x <= this.num)){
+        this.num++;
+        this.getNum(x);
+      }
+    }
+      this.arr.sort((left, right) => left-right);
+      return Number(this.arr[this.arr.length-1]);
+     
+    }
+  }
+  
+    
+  var sN = new SetNum;
+  
+  var promtFactor = sN.getNum (base);
+
   let count = 0;
-  let n = Math.trunc(number/5);
+  let n = Math.trunc(number/promtFactor);
   //if (n=0) {return 0;}
   while(n > 0) {
     count = count + n;
-    n = Math.trunc(n/5);
+    n = Math.trunc(n/promtFactor);
   }
  
-  let c=0, d=0;
-  let result = "";
+ 
 
- if (base<40) {
-    return(count.toString(base));
-   
- }
-  
-   while(count>=1){
-           c=Math.floor(count/base);
-           d=count%base;
-           result = d+result;
-         
-           count=c;
-        }
-
-  
-
-  return Number(result);
+  return count;
 }
